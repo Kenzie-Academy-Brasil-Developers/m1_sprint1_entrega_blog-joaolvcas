86213fffb5fd9659a86ba2d24abc17faa3b7739b
João Lucas <joacamilo.br@gmail.com>
2022-09-21 23:01:12 -0300

    Add Model Complete

---
+++ ATIVIDADES/M1/S3 (JAVASCRIPT)/script.js
@@ -0,0 +1,11 @@
+function contaPassos(passos){
+    let i = 1
+    let passos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    
+    while(i < passos.length)
+
+      console.log(`Visitei a casa ${i} `)
+       
+      i++
+       
+}
